8d0d07e48e90baf7b3969321c12dd2a84de4ce6c
joshnieuwstad <josh.nieuwstad@bloxs.com>
2025-05-18 21:58:55 +0200

    align main page

--- src/components/Listings.tsx
+++ src/components/Listings.tsx
@@ -126,7 +126,7 @@ export default function Listings({ Airports }: AirportResponse) {
 
         </div>
       </form>
-      <div className="max-w-6xl mx-auto p-4 md:px-6 md:py-14">
+      <div className="max-w-6xl mx-auto py-4 md:py-14">
         {loading ?
           (
             <Loader />
